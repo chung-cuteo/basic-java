3c219f8ead074fd8daff837434d382b8c3f72afe
chung-cuteo <dinhquangchung.k9sh@gmail.com>
2024-04-12 21:21:09 +0900

    first commit

---
+++ src/bai1.java
@@ -0,0 +1,22 @@
+public class bai1 {
+  public static void main(String[] args) {
+    System.out.println("dong 1");
+    System.out.println("dong 2");
+
+    System.out.println("--------------");
+    // print
+    System.out.print("dong 1");
+    System.out.print("dong 2");
+    
+    System.out.println("--------------");
+
+    //  \t => thut vao tab
+    System.out.println("dong 1");
+    System.out.println("\tdong 2");
+    System.out.println("--------------");
+
+   //　\n => xuong dong
+   // \"" su dung nhay kep
+
+  }
+}
