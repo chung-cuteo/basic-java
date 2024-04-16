42bf21bcf4c6aa1b9558e8fe9fc20f564e08f153
chung-cuteo <dinhquangchung.k9sh@gmail.com>
2024-04-16 16:25:17 +0900

    add commit

---
+++ src/BaiClass.java
@@ -0,0 +1,7 @@
+public class BaiClass {
+  public static void main(String[] args) {
+    // khoi tao doi tuong sinh vien
+    SinhVien sVien1 = new SinhVien();
+    sVien1.hienThiThongTin();
+  }
+}
---
+++ src/SinhVien.java
@@ -0,0 +1,9 @@
+
+public class SinhVien {
+  private String hoTen;
+  private double diem;
+
+  public void hienThiThongTin() {
+    System.out.println(hoTen + " " + diem);
+  }
+}
